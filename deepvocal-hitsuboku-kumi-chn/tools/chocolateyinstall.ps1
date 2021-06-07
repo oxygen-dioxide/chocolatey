3212edca85e1bef81975c3360ca10c469abb9c48
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2021-06-07 19:03:21 +0800

    -

--- deepvocal-hitsuboku-kumi-chn/tools/chocolateyinstall.ps1
+++ deepvocal-hitsuboku-kumi-chn/tools/chocolateyinstall.ps1
@@ -1,15 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
  
-$url        = 'https://github.com/oxygen-dioxide/hitsuboku-kumi-chn-deepvocal/releases/download/v1.0.0/Hitsuboku-Kumi-Chinese-deepvocal-1.0.0.exe'
+$url        = 'https://github.com/oxygen-dioxide/hitsuboku-kumi-chn-deepvocal/releases/download/v1.1.0/Hitsuboku-Kumi-Chinese-deepvocal-1.1.0-dvtb2.exe'
 $packageArgs = @{
   packageName   = 'deepvocal-hitsuboku-kumi-chn'
   url           = $url
-  #unzipLocation = $toolsdir
   fileType      = 'EXE' #only one of these: exe, msi, msu
-  #file          = Join-Path $toolsdir 'Hitsuboku-Kumi-Chinese-deepvocal-1.0.0.exe'
   softwareName  = 'deepvocal-hitsuboku-kumi-chn*'
-  checksum      = '8f49dc3f72cef3f9465abc06d02e727bc20a16c8602c5dbc9980d1686cc6829b'
+  checksum      = '8ba38371d552717dc1c90115495ae57a1d54dbc04a4197068db6a0bf0d1fdcff'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
   validExitCodes= @(0, 3010, 1641)
--- xstudio/tools/chocolateyinstall.ps1
+++ xstudio/tools/chocolateyinstall.ps1
@@ -1,19 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
- 
-$url        = 'https://singingtool.blob.core.chinacloudapi.cn/singingtool-container/XStudioSinger_2021-04-02_1.5.0.zip'
-$packageArgs = @{
-  packageName   = 'xstudio'
-  url           = $url
-  unzipLocation = $toolsdir
-  fileType      = 'EXE' #only one of these: exe, msi, msu
-  file          = Join-Path $toolsdir 'XStudioSinger_installer_1.5.0.exe'
- # softwareName  = 'deepvocal*'
-  checksum      = 'dd0ebe82c8acdf0173526fa22e0ceca511e606ac4a7fe186572893d1b143fdd6'
-  checksumType  = 'sha256'
-  silentArgs    = "/S"
-  validExitCodes= @(0)
-}
- 
-Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyInstallPackage @packageArgs
+
+$packageName = 'xstudio'
+$url = 'https://singingtool.blob.core.chinacloudapi.cn/singingtool-container/XStudioSinger_2021-04-02_1.5.0.zip'
+$checksum = 'dd0ebe82c8acdf0173526fa22e0ceca511e606ac4a7fe186572893d1b143fdd6'
+$checksumType = 'sha256'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$File = Join-Path $toolsdir 'XStudioSinger_installer_1.5.0.exe'
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation -checksum $checksum -checksumType $checksumType -checksumType64 $checksumType
+Get-ChocolateyUnzip $File $unzipLocation
