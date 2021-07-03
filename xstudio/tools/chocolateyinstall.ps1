238a1c49ef47f4abe4e2269414b5272b15553c7f
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2021-07-03 11:45:34 +0800

    xs1.7.2

--- xstudio/tools/chocolateyinstall.ps1
+++ xstudio/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageName = 'xstudio'
-$url = 'https://singingtool.blob.core.chinacloudapi.cn/singingtool-container/XStudioSinger_2021-04-02_1.5.0.zip'
-$checksum = 'dd0ebe82c8acdf0173526fa22e0ceca511e606ac4a7fe186572893d1b143fdd6'
+$url = 'https://singingtool.blob.core.chinacloudapi.cn/singingtool-container/XStudioSinger_2021-07-02_1.7.2.zip'
+$checksum = '7426313cd63f2c1d128ccd565096343fe249526b81e2122bf1ac8f6fc582fad0'
 $checksumType = 'sha256'
 $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$File = Join-Path $toolsdir 'XStudioSinger_installer_1.5.0.exe'
+$File = Join-Path $toolsdir 'XStudioSinger_installer_1.7.2.exe'
 
 Install-ChocolateyZipPackage $packageName $url $unzipLocation -checksum $checksum -checksumType $checksumType -checksumType64 $checksumType
 Get-ChocolateyUnzip $File $unzipLocation
