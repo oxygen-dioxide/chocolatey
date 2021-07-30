e85a008659405c6b5ed62f01d6d0a5e9ff416ea2
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2021-07-30 22:22:44 +0800

    vogen

---
+++ vogen/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'vogen'
+$url = 'https://github.com/aqtq314/Vogen.Client/releases/download/v0.1.1.4/Vogen.Client.v0.1.1.4.zip'
+$checksum = '9d665aa19e1bfb05096300be999eba4c3c8d91e92960353ae57f369959704cc2'
+$checksumType = 'sha256'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation -checksum $checksum -checksumType $checksumType -checksumType64 $checksumType
