3d6b790a1163e292c29e5bd70c097f0d21e6c356
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2021-08-19 17:34:10 +0800

    openutau

---
+++ openutau/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'openutau'
+$url = 'https://github.com/stakira/OpenUtau/releases/download/OpenUtau-Latest/OpenUtau.zip'
+$checksum = 'e9539bc8aac2e544745189f32be35fdf33dcf6a31930fe42f17cf289c51f43b6'
+$checksumType = 'sha256'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation -checksum $checksum -checksumType $checksumType -checksumType64 $checksumType
