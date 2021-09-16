12373eec2a63b92a3d8c87a12a37cb2d5635cf3f
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2021-09-16 16:24:07 +0800

    utsu

---
+++ utsu/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+ 
+$url        = 'https://github.com/titinko/utsu/releases/download/v0.4.4/Utsu-0.4.4.msi'
+$packageArgs = @{
+  packageName   = 'utsu'
+  url           = $url
+  fileType      = 'msi' #only one of these: exe, msi, msu
+  softwareName  = 'Utsu*'
+  checksum      = '36a3533c8a74cdff651feef74133bb7cc27dbf2c60225531bd0b4e49ecc06e68'
+  checksumType  = 'sha256'
+  silentArgs    = "/qr"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
