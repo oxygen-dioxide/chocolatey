be188ef34bd8cb141e94b6e0fee4edb4e98738b7
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2021-09-16 16:41:12 +0800

    yanknote

---
+++ yanknote/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'yanknote'
+$url = 'https://github.com/purocean/yn/releases/download/v3.6.4/Yank-Note-win-x64-3.6.4.zip'
+$checksum = 'B1EC425FA5CA010EF5642BFF8E7D7228D0F33547D89B1913F82AECB96CE0F8CD'
+$checksumType = 'sha256'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation -checksum $checksum -checksumType $checksumType -checksumType64 $checksumType
