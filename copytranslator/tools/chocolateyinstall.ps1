ff17950d8527721139540ad289384a4cc96254e2
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2021-09-18 18:36:06 +0800

    copytranslator majsoul-plus

---
+++ copytranslator/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'copytranslator'
+$url = 'https://github.com/CopyTranslator/CopyTranslator/releases/download/v9.1.0/copytranslator-9.1.0-win.zip'
+$checksum = 'F635D340983009EC3F304279AA59E51A0F4401028B46AC08BB68D2045DC179EA'
+$checksumType = 'sha256'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation -checksum $checksum -checksumType $checksumType -checksumType64 $checksumType
---
+++ majsoul-plus/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'majsoul-plus'
+$url = 'https://github.com/MajsoulPlus/majsoul-plus/releases/download/v2.0.0/Majsoul_Plus-2.0.0-win32-ia32.zip'
+$checksum = '0C049A6723E1D80F74C44DFC921A0D05269F0286F8611C3D4ED9E89C5FFB2AC7'
+$checksumType = 'sha256'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation -checksum $checksum -checksumType $checksumType -checksumType64 $checksumType
