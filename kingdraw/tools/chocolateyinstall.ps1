83c10fd6663e48f5ce2b6f3d706ee256987e5a74
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2021-09-20 10:26:08 +0800

    kingdraw

---
+++ kingdraw/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageName = 'kingdraw'
+$url = 'http://windows.download.kingdraw.cn/KingDrawPc_V2.1.0.00-zh-cn.exe'
+$checksum = 'E0769355C94CAAB5EBA7A4E6A58901CC8B247DD7A27F46470B2E09DF935EEB36'
+$checksumType = 'sha256'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$File = Join-Path $toolsdir 'app.7z'
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation -checksum $checksum -checksumType $checksumType -checksumType64 $checksumType
+Get-ChocolateyUnzip $File $unzipLocation
