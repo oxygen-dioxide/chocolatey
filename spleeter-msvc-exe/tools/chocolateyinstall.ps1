60e2706c5d94145c9daac9ef7355993dc21478ad
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2021-09-20 09:24:57 +0800

    spleeter-msvc-exe

---
+++ spleeter-msvc-exe/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'SpleeterMsvcExe'
+$url = 'https://github.com/wudicgi/SpleeterMsvcExe/releases/download/v1.0/SpleeterMsvcExe-v1.0.zip'
+$checksum = '9151D1DCB1B7E61626E6228E98FBE733EA187B475715D1C68F4A84CABC063396'
+$checksumType = 'sha256'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation -checksum $checksum -checksumType $checksumType -checksumType64 $checksumType
