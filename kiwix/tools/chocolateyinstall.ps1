6d672f8dd9b8e41427a2300acaf4ad6d6577facf
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2021-12-07 15:55:57 +0800

    kiwix

---
+++ kiwix/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'kiwix'
+$url = 'https://mirrors.dotsrc.org/kiwix/release/kiwix-desktop/kiwix-desktop_windows_x64_2.0.5.zip'
+$checksum = '33a378e202819419fd6c38cb2bbdd5c812ef421f9cf4d922ac008cac330a3e2a'
+$checksumType = 'sha256'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation -checksum $checksum -checksumType $checksumType -checksumType64 $checksumType
